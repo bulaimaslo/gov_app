4648af76c9faf0e412eb1cf2740c29e455b07e18
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2022-05-02 02:33:34 +0200

    Add deployed contract abi

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,101 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import abi from "./utils/Governance.json";
 
-function App() {
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [proposal, setProposal] = useState();
+
+  const contractAddress = "0x91A2D9dE7C0662f3C947ca6ae1b097F5E3fd1F4F";
+  const contractABI = abi.abi;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getProposal = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const governance = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const proposal = await governance.proposal()
+
+        setProposal(proposal);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Governance App
-        </p>
-      </header>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">Governance app</div>
+
+        {!currentAccount && (
+          <button className="connectWalletButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
